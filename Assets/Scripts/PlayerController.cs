c5fd6b73f4aa52579b7e499f32eb9e1f2d34701f
sonkiboy <kratzharrison@gmail.com>
2024-01-28 15:02:06 -0600

    Added Model Rotation
    Player model will now rotate based on the direction the player is moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,10 +24,12 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        // initialize the player actions by creating a new instance
         playerControls = new FeywoodPlayerActions();
     }
     private void OnEnable()
     {
+        // enable move input system
         move = playerControls.Player.Move;
         move.Enable();
     }
@@ -56,16 +58,25 @@ public class PlayerController : MonoBehaviour
         // update the direction the player is trying to move based on the move input action
         moveDirection = move.ReadValue<Vector2>();
 
-        Debug.Log($"Move direction = {moveDirection}");
+
+        if(moveDirection != Vector2.zero)
+        {
+            model.transform.rotation = Quaternion.LookRotation(new Vector3(-moveDirection.x, 0, -moveDirection.y));
+        }
+        
+
+        //Debug.Log($"Move direction = {moveDirection}");
     }
 
     private void FixedUpdate()
     {
         
-
+        // calculate the new position based on the move direction, speed, and delta time
         Vector3 newPos = new Vector3(moveDirection.y * -Speed * Time.deltaTime, 0f, moveDirection.x * Speed * Time.deltaTime);
 
         //Debug.Log(newPos);
+
+        // rb.MovePosition used to move the object while accounting for any collisions made
         rb.MovePosition(rb.position + newPos);
     }
 }
