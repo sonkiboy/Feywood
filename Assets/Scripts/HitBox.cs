cc5ffc96967d4b2cf2fe520ec65c41ebbe44d80b
mollarc <mollark@live.com>
2024-02-23 14:30:54 -0600

    aaaaa

--- Assets/Scripts/HitBox.cs
+++ Assets/Scripts/HitBox.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Hitbox : MonoBehaviour
@@ -9,46 +10,66 @@ public class Hitbox : MonoBehaviour
     public GameObject TargetObject;
     
     public List<GameObject> TargetObjects = new List<GameObject>();
+    [SerializeField] Collider[] foundColliders = new Collider[0];
 
     [SerializeField] List<string> CollisionTags;
+
+    Collider collider;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        foundColliders = Physics.OverlapBox(collider.bounds.center, collider.bounds.size);
 
+        TargetObjects.Clear();
+
+        foreach (Collider collider in foundColliders)
+        {
+            //Debug.Log($"{this.name} collided with {collision.gameObject.name}");
+            if (CollisionTags.Contains(collider.gameObject.tag))
+            {
+                TargetObjects.Add(collider.gameObject);
+            }
+        }
+
+        
+        OnTargetUpdate();
+        
     }
+
+
 
     private void OnTriggerEnter(Collider collision)
     {
-        //Debug.Log($"{this.name} collided with {collision.gameObject.name}");
-        if (CollisionTags.Contains(collision.gameObject.tag))
-        {
-            TargetObjects.Add(collision.gameObject);
-        }
+        ////Debug.Log($"{this.name} collided with {collision.gameObject.name}");
+        //if (CollisionTags.Contains(collision.gameObject.tag))
+        //{
+        //    TargetObjects.Add(collision.gameObject);
+        //}
         
 
 
-        OnTargetUpdate();
+        //OnTargetUpdate();
 
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        if (TargetObjects.Contains(collision.gameObject))
-        {
-            TargetObjects.Remove(collision.gameObject);
-        }
+        //if (TargetObjects.Contains(collision.gameObject))
+        //{
+        //    TargetObjects.Remove(collision.gameObject);
+        //}
         
 
         
 
-        OnTargetUpdate();
+        //OnTargetUpdate();
         
     }
 
