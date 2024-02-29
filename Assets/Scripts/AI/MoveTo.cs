0c13ff8f0be56f8370792e34e1b630170727acf4
snortableCola <113148248+snortableCola@users.noreply.github.com>
2024-02-29 15:36:02 -0600

    Worked on a script that allows AI to patrol between points, all while avoiding obstacles

--- Assets/Scripts/AI/MoveTo.cs
+++ Assets/Scripts/AI/MoveTo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIMovement : MonoBehaviour
+public class MoveTo : MonoBehaviour
 {
     //creating an end goal for the NPC to move to
     public Transform goal;
