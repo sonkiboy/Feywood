d038ff268bd6d2d12614282932e949ef72a52a6c
snortableCola <113148248+snortableCola@users.noreply.github.com>
2024-02-29 13:53:42 -0600

    Added AI navigation package to project, created AI test scene, got an npc to move from one location to another while avoiding obstacles

---
+++ Assets/Scripts/AI/AIMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    //creating an end goal for the NPC to move to
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //connecting the Transform goal to the agent's end pos
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+   
+    
+}
