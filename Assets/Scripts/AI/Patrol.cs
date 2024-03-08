9613ddfcaf78f5d580d2aebd76ec02c70dc6402a
snortableCola <legocrazy2099@gmail.com>
2024-03-08 13:32:15 -0600

    changed sister speed and turned off autobraking

--- Assets/Scripts/AI/Patrol.cs
+++ Assets/Scripts/AI/Patrol.cs
@@ -21,7 +21,7 @@ public class Patrol : MonoBehaviour
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
         // approaches a destination point).
-        agent.autoBraking = true;
+        agent.autoBraking = false;
 
         //if (agentCollider.gameObject.CompareTag("Player"))
         //{
