6998a42f924dfa28e706c136e0ef7909d6444935
samanthag168 <90341346+samanthag168@users.noreply.github.com>
2024-04-05 14:32:25 -0500

    player can't move when the game is paused

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -31,13 +31,13 @@ public class PauseMenu : MonoBehaviour
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
-       
+        Time.timeScale = 0f;
         isPaused = true;
     }
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
-    
+        Time.timeScale = 1f; 
         isPaused = false;
         Debug.Log("game resumed");
 
