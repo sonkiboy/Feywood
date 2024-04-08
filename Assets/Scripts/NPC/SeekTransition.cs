540dd6786305d1cd38022f5920ac3296c42cbf4d
snortableCola <113148248+snortableCola@users.noreply.github.com>
2024-04-08 00:34:05 -0500

    stared creating a new script to teleport sister

---
+++ Assets/Scripts/NPC/SeekTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using DialogueUI;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+using Color = UnityEngine.Color;
+public class SeekTransition : MonoBehaviour
+{
+    public Transform hideSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider teleport)
+    {
+        //GameObject.Destroy(gameObject.name.<"Sister">);
+    }
+}
