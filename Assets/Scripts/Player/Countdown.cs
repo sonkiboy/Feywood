40b5c9a0fa7a2d0b64bb2e19524b896bbed533f4
mollarc <mollark@live.com>
2024-05-05 19:54:26 -0500

    Script Updated to prevent build errors

--- Assets/Scripts/Player/Countdown.cs
+++ Assets/Scripts/Player/Countdown.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using DialogueUI;
@@ -63,3 +64,4 @@ public class Countdown : MonoBehaviour
         yield return null;
     }
 }
+#endif
